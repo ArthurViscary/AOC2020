66277c67cdd1e0d9dd5f2ab23ce760b1ea016f81
ArthurViscary <rantanen.villes@gmail.com>
2021-11-21 20:43:28 +0200

    day 3 done

--- aoc2020-day3/aoc2020-day3.py
+++ aoc2020-day3/aoc2020-day3.py
@@ -13,4 +13,25 @@ while row + 1 < len(data):
     if space == "#":
         tree_count += 1
 
-print(tree_count)
+#part 2
+def part2_checker(data:list) -> int:
+    slope_variations = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    tree_count_totals = []
+    def tree_counter(data:list, slope_variation:list) -> int:
+        tree_count = 0
+        row, column = 0,0
+        while row + 1 < len(data):
+            row += int(slope_variation[1])
+            column += int(slope_variation[0])
+    
+            space = data[row][column % len(data[row])]
+            if space == "#":
+                tree_count += 1 
+        return tree_count
+    for slope in slope_variations:
+        tree_count_totals.append(tree_counter(data, slope))
+    final_totals = 1
+    for item in tree_count_totals:
+        final_totals *= item
+    return final_totals
+
