2e22d106447a7552c93dc14ee9f5ca9f59de7665
ArthurViscary <rantanen.villes@gmail.com>
2021-11-21 20:08:54 +0200

    part 2 done

--- aoc2020-day1/aoc2020-day1.py
+++ aoc2020-day1/aoc2020-day1.py
@@ -5,6 +5,7 @@ with open("aoc2020-day1input.txt") as input:
     data = input.readlines()
     data = [line.strip() for line in data]
 
+#part1
 def checker(data:list) -> int:
     count = 0
     for row in data:
@@ -14,3 +15,22 @@ def checker(data:list) -> int:
             count += 1
     return count
 
+#part2
+def part2_checker(data:list) -> int:
+    count = 0
+    for row in data:
+        result_groups = re.search(r"(\d+)-(\d+) (\w): (\w+)", row)
+        positions = 0
+        index1 = int(result_groups.group(1)) - 1
+        index2 = int(result_groups.group(2)) - 1
+        char = result_groups.group(3)
+        password = result_groups.group(4)
+        if password[index1] == char:
+            positions += 1
+        if password[index2] == char:
+            positions += 1
+        if positions == 1:
+            count += 1
+    return count
+
+print(part2_checker(data))
