5cc8243ec5a25b2e1e7787f24fedb45ae97279c6
ArthurViscary <rantanen.villes@gmail.com>
2021-11-22 11:02:53 +0200

    day 6 part 1

---
+++ aoc2020-day6/aoc2020-day6.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+with open("aoc2020-day6input.txt") as input:
+    data = input.read().split("\n\n")
+
+sum = 0
+
+
+#notice: every line but one had a newline char, which we want to ignore
+for line in data:
+    if '\n' not in Counter(line):
+        sum += 1
+    sum += len(Counter(line)) - 1
+
+print(sum)
