d465b073f5674204c34d3e3bcd35ce7e431521a0
ArthurViscary <rantanen.villes@gmail.com>
2021-12-13 20:31:13 +0200

    day6 pt 2

--- aoc2020-day4/aoc2020-day4.py
+++ aoc2020-day4/aoc2020-day4.py
@@ -3,6 +3,7 @@
 with open("aoc2020-day4input.txt") as input:
     data = input.read().split("\n\n")
 
+#pt 1
 def validity_checker(passport:str) -> bool:
     fields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
     for item in fields:
@@ -14,4 +15,17 @@ valid_passports = 0
 for passport in data:
     if validity_checker(passport):
         valid_passports += 1
-print(valid_passports)
+#print(valid_passports)
+
+def pt2_validity_checker(passport:str) -> bool:
+    fields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
+    for item in fields:
+        if item not in passport:
+            return False
+    for item in fields:
+        if item == 'byr' and 'byr' < 1920 or 'byr' > 2002:
+            return False
+        if item == 'iyr' and 'iyr' < 2010 or 'iyr' > 2020:
+            return False
+        if item == 'eyr' and 'eyr'
+    return True
--- aoc2020-day6/aoc2020-day6.py
+++ aoc2020-day6/aoc2020-day6.py
@@ -11,3 +11,18 @@ for line in data:
     if '\n' not in Counter(line):
         sum += 1
     sum += len(Counter(line)) - 1
+
+
+#pt2
+def pt2_solver(data:list[str]) -> int:
+    pt2_sum = 0
+    for line in data:
+        answers = len(line.split('\n'))
+        line_counter = Counter(line)
+        for char in line_counter:
+            if char != '\n' and line_counter.get(char) == answers:
+                pt2_sum += 1
+    return pt2_sum
+
+
+print(pt2_solver(data))
